b4a1720fcfba42ccb01d17a469f35ad33108e7dd
enamul-007 <anamulkhan523@gmail.com>
2024-06-02 10:08:52 +0600

    nav bar add

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('.main_slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-chevron-left left"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right right"></i>',
+});
